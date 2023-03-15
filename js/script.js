bd880c964f9de1d48f833aa24627178eaeb5016d
mykhailo-svg <74141693+mykhailo-svg@users.noreply.github.com>
2023-03-16 00:18:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const burgerButton = document.querySelector('.burger__button');
+
+const burgerBody = document.querySelector('.burger__body');
+
+const burgDecor = document.querySelector('.burger__decor')
+
+const burgerButtonDecors = document.querySelectorAll('.burger__button__decor');
+
+
+const mainBody = document.querySelector('body');
+
+
+burgerButton.addEventListener('click', function ()  {
+
+    mainBody.classList.toggle('body_noScroll');
+    burgDecor.classList.toggle('burger__dec__active');
+    
+    
+
+    if(burgerBody.classList.length<2){
+        burgerBody.classList.toggle('burger__show');
+        
+    }
+    else{
+        burgerBody.classList.toggle('burger__hide');
+        burgerBody.classList.toggle('burger__show');
+
+    }
+   
+    for (let i = 0; i < burgerButtonDecors.length; i++) {
+        burgerButtonDecors[i].classList.toggle('burger__dec__active');
+        
+    }
+    
+});
